2a183df8922c31fbd65b282da1701f7dec706e34
Ivan Lim <uxprogrammer@gmail.com>
2022-05-15 13:20:29 +0800

    display logo and create navbar links

--- frontend_react/src/components/NavBar/NavBar.jsx
+++ frontend_react/src/components/NavBar/NavBar.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+import { images } from "../../constants";
 import "./NavBar.scss";
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <nav>
+      <div>
+        <img src={images.logo} alt="logo" />
+      </div>
+      <ul>
+        {["home", "about", "contact", "work", "skills"].map((item) => (
+          <li key={`link-${item}`}>
+            <div />
+            <a href={`#${item}`}>{item}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 };
 
 export default NavBar;
